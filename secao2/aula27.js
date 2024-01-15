0c409c04de86ce632e2b8e2ddea244ee9a729f54
Walker Negrao <1882452@tcs.com>
2024-01-15 16:42:40 -0300

    aula27 - Implementada

---
+++ secao2/aula27.js
@@ -0,0 +1,6 @@
+let min = 5;
+let max = 10;
+
+
+console.log(Math.random() * (max - min) + min); // Gera um número double aleatório entre 5 e 10
+console.log(Math.round(Math.random() * (max - min) + min)); // Gera um número inteiro aleatório entre 5 e 10
